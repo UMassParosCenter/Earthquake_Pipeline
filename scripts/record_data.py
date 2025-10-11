36e8d0648ce73439748a871e107289027ef048eb
Xuanyi Zhang <58002595+yizhang24@users.noreply.github.com>
2025-10-10 20:16:34 -0400

    Add constants for window length, window overlap, and sample rate

--- scripts/record_data.py
+++ scripts/record_data.py
@@ -6,21 +6,24 @@ from scripts.constants import (
     EARTHQUAKE_LOG_PATH,
     BACKGROUND_BUFFER_HOURS,
     N_BACKGROUND_SAMPLES,
-    WINDOW_BEFORE_SEC,
-    WINDOW_AFTER_SEC,
+    EVENT_BEFORE_SEC,
+    EVENT_AFTER_SEC,
+    SAMPLE_RATE_HZ,
+    WINDOW_OVERLAP,
+    WINDOW_LENGTH_SEC,
     BACKGROUND_PSD_PKL,
     EARTHQUAKE_PSD_PKL
 )
 
 box = event_catalog_utils.load_box_config(BOX_CONFIG_PATH)
 print("Generating background data")
-background_windows = event_catalog_utils.generate_background_data(EARTHQUAKE_LOG_PATH, box, BACKGROUND_BUFFER_HOURS, N_BACKGROUND_SAMPLES, WINDOW_BEFORE_SEC, WINDOW_AFTER_SEC)
-earthquake_windows = event_catalog_utils.generate_earthquake_data(EARTHQUAKE_LOG_PATH, box, WINDOW_BEFORE_SEC, WINDOW_AFTER_SEC)
+background_windows = event_catalog_utils.generate_background_data(EARTHQUAKE_LOG_PATH, box, BACKGROUND_BUFFER_HOURS, N_BACKGROUND_SAMPLES, EVENT_BEFORE_SEC, EVENT_AFTER_SEC)
+earthquake_windows = event_catalog_utils.generate_earthquake_data(EARTHQUAKE_LOG_PATH, box, EVENT_BEFORE_SEC, EVENT_AFTER_SEC)
 print("Processing background data")
-all_bg_psds, labeled_bg_dicts = background_utils.process_background_data(background_windows, box.sample_rate_hz, 100, 0.5, 10)
+all_bg_psds, labeled_bg_dicts = background_utils.process_background_data(background_windows, box.sample_rate_hz, SAMPLE_RATE_HZ, WINDOW_OVERLAP, WINDOW_LENGTH_SEC)
 any_window = list(list(labeled_bg_dicts.values())[0].values())[0]
 baseline = background_utils.build_simple_baseline(all_bg_psds, any_window["frequency"])
-eq_data = earthquake_utils.process_earthquake_data(earthquake_windows, box.sample_rate_hz, 100, 0.5, 10, baseline)
+eq_data = earthquake_utils.process_earthquake_data(earthquake_windows, box.sample_rate_hz, SAMPLE_RATE_HZ, WINDOW_OVERLAP, WINDOW_LENGTH_SEC, baseline)
 with open(BACKGROUND_PSD_PKL, "wb") as f:
     pickle.dump(labeled_bg_dicts, f)
 with open(EARTHQUAKE_PSD_PKL, "wb") as f:
